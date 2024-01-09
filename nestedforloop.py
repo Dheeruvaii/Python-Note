13ba4bb04cc340c7238b4c4e90d6bf60bfe32420
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-09 22:25:30 +0545

    nested while loop

--- nestedforloop.py
+++ nestedforloop.py
@@ -1,6 +1,21 @@
 # for print rows(*)
-rows=5
-for i in range(rows+1):
-    for j in range(i):
-        print('*',end='')
-    print('')
+# rows=5
+# for i in range(rows+1):
+#     for j in range(i):
+#         print('*',end='')
+#     print('')
+
+
+
+# # While Loop inside list nested loop
+# names=['A','B','C']
+
+# for name in names:
+
+#     count= 0
+#     while count < 5:
+#         print(name, end='')
+#         count = count + 1
+
+#     print()
+    
