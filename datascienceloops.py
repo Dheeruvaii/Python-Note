98b4d13ccbba075f89a045af1e5855f01f1501a7
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-11 17:29:05 +0545

    pandas completed

--- datascienceloops.py
+++ datascienceloops.py
@@ -6,7 +6,28 @@ data = {'Name': ['John', 'Jane', 'Mike'],
         'City': ['New York', 'London', 'Paris']}
 
 df = pd.DataFrame(data)
+add_data ={'NewName':['Alice']}
 
-# Iterate over rows
+new={'data':df}
+new.update({"add_data":pd.DataFrame(add_data)})
 for index, row in df.iterrows():
     print(row['Name'], row['Age'], row['City'])
+
+print(new["add_data"])
+# import pandas as pd
+
+# data = {'Name': ['John', 'Jane', 'Mike'],
+#         'Age': [25, 30, 35],
+#         'City': ['New York', 'London', 'Paris']}
+
+# df = pd.DataFrame(data)
+
+# # Existing dictionary
+# existing_data = {"existing_data": "some_value"}
+
+# # Append DataFrame to the existing dictionary
+# existing_data.update({"new_data": df})
+
+# # Iterate over rows
+# for index, row in df.iterrows():
+#     print(row['Name'], row['Age'], row['City'])
--- operators.py
+++ operators.py
@@ -30,14 +30,3 @@ print(0 | 3)
 print(0 & 3)
 
 
-import pandas as pd
-
-data = {'Name': ['John', 'Jane', 'Mike'],
-        'Age': [25, 30, 35],
-        'City': ['New York', 'London', 'Paris']}
-
-df = pd.DataFrame(data)
-
-# Iterate over rows
-for index, row in df.iterrows():
-    print(row['Name'], row['Age'], row['City'])
