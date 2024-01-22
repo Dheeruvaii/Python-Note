768a14eb181199c835f297c919a36df59abe3fb4
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-22 23:37:48 +0545

    use of *args and **kwargs

--- args and kwargs.py
+++ args and kwargs.py
@@ -42,3 +42,11 @@ def my_sum(my_integers):
 
 list_of_integers = [1, 2, 3]
 print(my_sum(list_of_integers))
+def my_sum(*args):
+    result = 0
+    # Iterating over the Python args tuple
+    for x in args:
+        result += x
+    return result
+
+print(my_sum(1, 2, 3))
