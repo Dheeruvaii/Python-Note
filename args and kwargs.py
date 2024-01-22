3557c10ae573cfff4137a0a623d2a88ba32ee103
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-22 23:24:54 +0545

    access module written in py from c

--- args and kwargs.py
+++ args and kwargs.py
@@ -25,3 +25,12 @@ mb = car(s=190, c='white')
 # printing the color and speed of cars
 print(audi.color)
 print(bmw.speed)
+
+"""How do I access a module written in Python from C?"""
+# Answer:
+
+# Understanding how Python works is a given for any Python developer. However, there’s no point in trying to get a word-for-word answer. Instead, look out for personal stories on how candidates have used specific features. 
+
+# You can get a pointer to the module object as follows:
+
+# module = PyImport_ImportModule("");
