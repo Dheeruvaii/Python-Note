01987420603b17ba0c38f0c3b5a2fa3ae00ba6c7
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-22 23:08:20 +0545

    use of *args

--- list swap.py
+++ list swap.py
@@ -1,13 +1,19 @@
 #  Find the length of the list and simply swap the first element with (n-1)th element.
 
-def swaplist(list):
-    first=list.pop(0)
-    last=list.pop(-1)
+# def swaplist(list):
+#     first=list.pop(0)
+#     last=list.pop(-1)
 
-    list.insert(0,last)
-    list.append(first)
-    print(swaplist)
-    return list
+#     list.insert(0,last)
+#     list.append(first)
+#     print(swaplist)
+#     return list
 
-new_list=[12,13,14,15,16]
-print(swaplist(new_list))
+# new_list=[12,13,14,15,16]
+# print(swaplist(new_list))
+ 
+num=[1,2,3,4,5]
+print(*num)
+# num=num+1
+# print(**num)
+
