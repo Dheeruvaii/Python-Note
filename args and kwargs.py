3c48c674e097bf0faf58045e9e1f781fb391ee86
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-22 23:13:26 +0545

    use of *args and **kwargs

--- args and kwargs.py
+++ args and kwargs.py
@@ -7,3 +7,21 @@ def myFun(*args, **kwargs):
 # Now we can use both *args ,**kwargs
 # to pass arguments to this function :
 myFun('geeks', 'for', 'geeks', first="Geeks", mid="for", last="Geeks")
+
+# defining car class
+class car():
+	# args receives unlimited no. of arguments as an array
+	def __init__(self, **kwargs):
+		# access args index like array does
+		self.speed = kwargs['s']
+		self.color = kwargs['c']
+
+
+# creating objects of car class
+audi = car(s=200, c='red')
+bmw = car(s=250, c='black')
+mb = car(s=190, c='white')
+
+# printing the color and speed of cars
+print(audi.color)
+print(bmw.speed)
