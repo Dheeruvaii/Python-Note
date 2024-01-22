d4732c1f049a69c9efc2aeac8832462dee6fb610
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-22 23:12:37 +0545

    use of *args and **kwargs

---
+++ args and kwargs.py
@@ -0,0 +1,9 @@
+
+def myFun(*args, **kwargs):
+    print("args: ", args)
+    print("kwargs: ", kwargs)
+ 
+ 
+# Now we can use both *args ,**kwargs
+# to pass arguments to this function :
+myFun('geeks', 'for', 'geeks', first="Geeks", mid="for", last="Geeks")
