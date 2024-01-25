84516ded8f4988793640176eedebd615314a085e
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-25 19:27:09 +0545

    python inheritance

--- pythoncompositions.py
+++ pythoncompositions.py
@@ -12,4 +12,28 @@ my_car=Car()
 my_car.engine.start()
 
 
+class Dog:
+    def __init__(self,name):
+        self.name=name
+
+    def bark(self):
+        print(f"sounds woof")
+my_dog=Dog('puppy')
+my_dog.bark()
+
+
+class Animal:
+    def __init__(self,name):
+        self.name=name
+        pass
+    def speak(self,name):
+         pass
+
+class Dog(Animal):
+    def speak(self):
+        print(f"{self.name} woof!")
+
+my_dog=Dog('jhon')
+my_dog.speak()
+
 
