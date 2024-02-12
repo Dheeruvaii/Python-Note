315747c53cf1f39adc47cff7b41f36ea92c9e96c
Dhiraj Sapkota <101037569+Dheeruvaii@users.noreply.github.com>
2024-02-12 11:36:12 +0000

    code update

--- randomQprac.py
+++ randomQprac.py
@@ -3,6 +3,8 @@
 
 my_list=[1,2,3,6,5]
 my_list.sort()
+my_list.remove(2)
+my_list.pop(2)
 print(my_list)
 
 """reverse a string"""
@@ -12,3 +14,9 @@ for i in str1:
     str2 = i + str2
 print("The original string is: ", str1)
 print("The reversed string is: ", str2)
+
+
+
+"""remove a file"""
+# import os
+# os.remove("txt1.txt")
