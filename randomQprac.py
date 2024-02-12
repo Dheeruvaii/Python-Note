40dfe1bd2b84f7743d821531c4b8c0e41ff38062
Dhiraj Sapkota <101037569+Dheeruvaii@users.noreply.github.com>
2024-02-12 11:33:01 +0000

    update list sort and string reverse

---
+++ randomQprac.py
@@ -0,0 +1,14 @@
+
+"""sort a list elements"""
+
+my_list=[1,2,3,6,5]
+my_list.sort()
+print(my_list)
+
+"""reverse a string"""
+str1 = "Analytics Vidhya"
+str2 = ""
+for i in str1:
+    str2 = i + str2
+print("The original string is: ", str1)
+print("The reversed string is: ", str2)
