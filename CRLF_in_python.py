95de79cc50e98724bb29337d7a348c9a15cabc57
Dhiraj Sapkota <101037569+Dheeruvaii@users.noreply.github.com>
2024-02-14 05:54:52 +0000

    update crlf practice

---
+++ CRLF_in_python.py
@@ -0,0 +1,32 @@
+"""
+CRLF:Carriage Return(\r) Line Feeding (\n)
+"""
+
+# 1. Using only carriage return in Python
+my_string ='My first crlf practice \rFirst'
+print(my_string)
+
+# 2. Using carriage return in Python with a newline character
+
+string = 'My website is Latracal \r\nSolution'
+print(string)
+
+string = 'My first crlf practice \n\rFirst'
+print(string)
+
+string = 'My first c\nrlf practice \rFirst'
+print(string)
+
+
+# 3. carriage functions using crlf with tab
+"""
+In this example, we will be using the carriage or \r with the combination of tab space or \t in the program between the string.
+"""
+str= ('\tmyfirst \day')
+print(str) 
+
+
+# 4. Using carriage return in Python, tab space, and newline character
+str= ('\tmyfirst\rFirst\n\tmewday a\rewvibes\n')
+print(str)
+
