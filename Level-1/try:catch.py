8eb45a6607d1c8f1fc87e59a4f772bbeec02ed4b
Dhiraj Sapkota <101037569+Dheeruvaii@users.noreply.github.com>
2024-02-16 11:43:18 +0000

    practice try catch

---
+++ Level-1/try:catch.py
@@ -0,0 +1,10 @@
+try:
+    a=int(input("enter a number"))
+    b=int(input("enter a number"))
+    num=a/b
+    print(f"division:{num}"
+    )
+except:
+    print("division-error")
+else:
+    print("division successful")
